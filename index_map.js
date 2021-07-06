29c865f88186564a73b5df30eb7975449fdadfe6
Basin <82095451+Basin-Ir@users.noreply.github.com>
2021-07-06 10:19:01 +0430

    Update index_map.js

--- index_map.js
+++ index_map.js
@@ -1,14 +1,14 @@
 
 	mapboxgl.accessToken = 'pk.eyJ1IjoiYmFzaW4tbGFiIiwiYSI6ImNrcWw3ajhsYzAxNHkydm9hZ3dyY2xkeHkifQ.L0S8kwfD0By5nbKKl5hKTw';
     var coordinates = document.getElementById('coordinates');
-    var map = new mapboxgl.Map({
-        container: 'map',
+    var map_Basin_DEM = new mapboxgl.Map({
+        container: 'map_Basin_DEM',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [0, 0],
         zoom: 2
     });
 
-    var canvas = map.getCanvasContainer();
+    var canvas = map_Basin_DEM.getCanvasContainer();
 
     var geojson = {
         'type': 'FeatureCollection',
@@ -32,7 +32,7 @@
         // Update the Point feature in `geojson` coordinates
         // and call setData to the source layer `point` on it.
         geojson.features[0].geometry.coordinates = [coords.lng, coords.lat];
-        map.getSource('point').setData(geojson);
+        map_Basin_DEM.getSource('point').setData(geojson);
     }
 
     function onUp(e) {
@@ -46,18 +46,18 @@
         canvas.style.cursor = '';
 
         // Unbind mouse/touch events
-        map.off('mousemove', onMove);
-        map.off('touchmove', onMove);
+        map_Basin_DEM.off('mousemove', onMove);
+        map_Basin_DEM.off('touchmove', onMove);
     }
 
-    map.on('load', function () {
+    map_Basin_DEM.on('load', function () {
         // Add a single point to the map.
-        map.addSource('point', {
+        map_Basin_DEM.addSource('point', {
             'type': 'geojson',
             'data': geojson
         });
 
-        map.addLayer({
+        map_Basin_DEM.addLayer({
             'id': 'point',
             'type': 'circle',
             'source': 'point',
@@ -69,33 +69,33 @@
 
         // When the cursor enters a feature in
         // the point layer, prepare for dragging.
-        map.on('mouseenter', 'point', function () {
-            map.setPaintProperty('point', 'circle-color', '#3bb2d0');
+        map_Basin_DEM.on('mouseenter', 'point', function () {
+            map_Basin_DEM.setPaintProperty('point', 'circle-color', '#3bb2d0');
             canvas.style.cursor = 'move';
         });
 
-        map.on('mouseleave', 'point', function () {
-            map.setPaintProperty('point', 'circle-color', '#3887be');
+        map_Basin_DEM.on('mouseleave', 'point', function () {
+            map_Basin_DEM.setPaintProperty('point', 'circle-color', '#3887be');
             canvas.style.cursor = '';
         });
 
-        map.on('mousedown', 'point', function (e) {
+        map_Basin_DEM.on('mousedown', 'point', function (e) {
             // Prevent the default map drag behavior.
             e.preventDefault();
 
             canvas.style.cursor = 'grab';
 
-            map.on('mousemove', onMove);
-            map.once('mouseup', onUp);
+            map_Basin_DEM.on('mousemove', onMove);
+            map_Basin_DEM.once('mouseup', onUp);
         });
 
-        map.on('touchstart', 'point', function (e) {
+        map_Basin_DEM.on('touchstart', 'point', function (e) {
             if (e.points.length !== 1) return;
 
             // Prevent the default map drag behavior.
             e.preventDefault();
 
-            map.on('touchmove', onMove);
-            map.once('touchend', onUp);
+            map_Basin_DEM.on('touchmove', onMove);
+            map_Basin_DEM.once('touchend', onUp);
         });
     });
