d00e979c43c77fdd9cabf655420b2846c7ebfd2f
Basin <82095451+Basin-Ir@users.noreply.github.com>
2022-10-13 21:16:30 +0330

    Update Run.js

--- Run.js
+++ Run.js
@@ -1,17 +1,21 @@
-$(function() {
-  $( "#button___" ).click(function() {
-    $( "#button___" ).addClass( "onclic", 250, validate);
-  });
+let btn = document.querySelector('button');
 
-  function validate() {
-    setTimeout(function() {
-      $( "#button___" ).removeClass( "onclic" );
-      $( "#button___" ).addClass( "validate", 450, callback );
-    }, 2250 );
-  }
-    function callback() {
-      setTimeout(function() {
-        $( "#button___" ).removeClass( "validate" );
-      }, 1250 );
-    }
-  });
+btn.addEventListener('click', function () {
+  // form submission starts
+  // button is disabled
+  btn.classList.add('spin');
+  btn.disabled = true;
+  
+  // This disables the whole form via the fieldset
+  btn.form.firstElementChild.disabled = true;
+  
+  // this setTimeout call mimics some asyncronous action
+  // you would have something else here
+  window.setTimeout(function () {
+    // when asyncronous action is done, remove the spinner
+    // re-enable button/fieldset
+    btn.classList.remove('spin');
+    btn.disabled = false;
+    btn.form.firstElementChild.disabled = false;
+  }, 4000);
+}, false);
